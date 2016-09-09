ebecb12b55bc11976cc7584a54c95e64a0f850e3
Roeland Jago Douma <roeland@famdouma.nl>
2016-09-09 12:57:01 +0200

    Add owner to access list
    Signed-off-by: Roeland Jago Douma <roeland@famdouma.nl>

--- lib/private/Share20/Manager.php
+++ lib/private/Share20/Manager.php
@@ -1193,7 +1193,7 @@ class Manager implements IManager {
 			$path = $path->getParent();
 		}
 
-		$users = [];
+		$users = [$owner => 'null'];
 		$public = false;
 		$remote = false;
 		foreach ($shares as $share) {
