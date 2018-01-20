ce26aac9e3c81914422e57065afd5c14ed215509
Abijeet <abijeetpatro@gmail.com>
2018-01-21 01:08:14 +0530

    Moves delete icon to the left of the Delete text.

--- apps/files/templates/list.php
+++ apps/files/templates/list.php
@@ -72,8 +72,8 @@
 			<th id="headerDate" class="hidden column-mtime">
 				<a id="modified" class="columntitle" data-sort="mtime"><span><?php p($l->t( 'Modified' )); ?></span><span class="sort-indicator"></span></a>
 					<span class="selectedActions"><a href="" class="delete-selected">
-						<span><?php p($l->t('Delete'))?></span>
 						<span class="icon icon-delete"></span>
+						<span><?php p($l->t('Delete'))?></span>
 					</a></span>
 			</th>
 		</tr>
--- apps/files_trashbin/templates/index.php
+++ apps/files_trashbin/templates/index.php
@@ -46,8 +46,8 @@
 				<a id="modified" class="columntitle" data-sort="mtime"><span><?php p($l->t( 'Deleted' )); ?></span><span class="sort-indicator"></span></a>
 				<span class="selectedActions">
 					<a href="" class="delete-selected">
-						<span><?php p($l->t('Delete'))?></span>
 						<span class="icon icon-delete"></span>
+						<span><?php p($l->t('Delete'))?></span>
 					</a>
 				</span>
 			</th>
