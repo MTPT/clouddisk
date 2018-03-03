ed59d1f209b0a4fb40882b14ebf04fc1b44ec29e
Julius Härtl <jus@bitgrid.net>
2018-03-03 17:51:57 +0100

    Use DRONE_REPO_BRANCH
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

--- tests/ui-regression/run-servers.sh
+++ tests/ui-regression/run-servers.sh
@@ -4,7 +4,7 @@ cleaninstall() {
     rm -fr data/*
     rm -f config/config.php
 }
-BASEBRANCH=master
+BASEBRANCH=${DRONE_REPO_BRANCH:-master}
 BASEPATH=/tmp/base
 REPOPATH=`realpath ../../`
 [ ! -d $BASEPATH ] && mkdir -p $BASEPATH
@@ -12,6 +12,7 @@ REPOPATH=`realpath ../../`
 (
     git clone --recursive https://github.com/nextcloud/server.git $BASEPATH
     cd $BASEPATH
+    git checkout $BASEBRANCH
     cleaninstall
     php -S 0.0.0.0:8080 & 
 )
