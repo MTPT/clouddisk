0014b1acc5eef670fd7d11b741f1dd332f7d36af
Julius Härtl <jus@bitgrid.net>
2018-03-20 17:39:19 +0100

    More waiting
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

--- tests/ui-regression/test/installSpec.js
+++ tests/ui-regression/test/installSpec.js
@@ -17,14 +17,12 @@ describe('install', function () {
 		this.timeout(50000);
 		// just run for one resolution since we can only install once
 		return helper.takeAndCompare(this, '/index.php',  async function (page) {
-			const login = await page.$('#adminlogin');
-			const password = await page.$('#adminpass');
-			await login.type('admin');
-			await password.type('admin');
+			const login = await page.type('#adminlogin', 'admin');
+			const password = await page.type('#adminpass', 'admin');
 			const inputElement = await page.$('input[type=submit]');
 			await inputElement.click();
-			return await page.waitForNavigation({waitUntil: 'networkidle2'});
-		}, { viewport: {w: 1920, h: 1080}});
+			return await page.waitForNavigation({waitUntil: 'networkidle0'});
+		}, { waitUntil: 'networkidle0', viewport: {w: 1920, h: 1080}});
 	});
 
 });
