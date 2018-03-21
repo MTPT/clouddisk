a45664d89887036e478e0847135f637bdc841127
Julius Härtl <jus@bitgrid.net>
2018-03-22 00:53:37 +0100

    Remove bash script for running tests
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

--- tests/ui-regression/run-tests.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Waiting for Nextcloud servers to become available"
-until curl --silent http://ui-regression-php-master > /dev/null && curl --silent http://ui-regression-php > /dev/null
-do
-    sleep 2
-done
-
-node runTests.js
